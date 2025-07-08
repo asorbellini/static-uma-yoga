05244f96ff27ff6bbe3012dd119a26fd0f9df236
ayelen <ayesorbellini@gmail.com>
2025-07-08 16:52:54 -0300

    Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,31 +3,25 @@ import UmaLogo from "./Logo";
 
 function Footer() {
   return (
-    <footer className="bg-white text-center py-6 px-4 text-sm text-oscuro">
-      <div className="flex flex-row">
-        <div className="flex-1">
-          <UmaLogo colorFill='#5c7c5c'classname="w-16 h-16"/>
+    <footer className="bg-white text-center p-4 text-sm text-oscuro">
+      <div className="flex flex-row items-center">
+        <div className="basis-1/4">
+          <UmaLogo colorFill='#2c2c2c'classname="w-16 h-16"/>
         </div>
-        <div className="flex-1">
-          <h1 className="text-lg uppercase underline">Link Utili</h1>
-          <a href="/retreat-e-workshop" className="hover:text-oscuro/80">
-            <p>Retreat e Workshop</p>
-          </a>
-          <a href="/contatti" className="hover:text-oscuro/80">
-            <p>Contatti</p>
-          </a>
+        <div className="basis-2/4">
+          <p className="text-center text-[#2c2c2c]">
+            ©{new Date().getFullYear()} UMĀ Yoga Retreats - All rights reserved || Site by <a href="https://samportafolio.vercel.app/">S.A.M.</a>
+          </p>
         </div>
-        <div className="flex-1">
-          <h1 className="text-lg uppercase underline w-">Seguici su</h1>
+        <div className="basis-1/4">
+          <p className="uppercase underline">Siguici su</p>
           <a href="https://www.instagram.com/uma.retreats/" target="_blank" rel="noopener noreferrer" className="inline-flex justify-center mt-2">
             <InstagramIcon />
           </a>
         </div>
 
       </div>
-      <p className="px-12 text-center">
-         {new Date().getFullYear()} UMĀ Yoga Retreats - All rights reserved || Site by <a href="https://samportafolio.vercel.app/">S.A.M.</a>
-      </p>
+      
     </footer>
   );
 }
