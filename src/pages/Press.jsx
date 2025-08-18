48335d07810e11fdce91b00cf8e2c7ee7f86ce19
ayelen <ayesorbellini@gmail.com>
2025-08-18 17:33:17 -0300

    Update press and media information

--- src/pages/Press.jsx
+++ src/pages/Press.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
 import Footer from "../components/Footer"
 import { getTextDate } from "../constants"
+
+function LinkPreview({url}) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.microlink.io?url=${url}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="justify-items-center items-center mb-2">
+      {data &&
+          <img src={data.image.url} alt={data.title} className="w-28 h-28 rounded-3xl"/>
+}
+    </div>
+  );
+}
 
 function Press() {
     return(
@@ -12,8 +31,40 @@ function Press() {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 px-16 py-12 justify-center text-center gap-3 bg-gradient-to-tr from-verdeOliva to-verdeBosque">
                 <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
-                    <div className="justify-items-center items-center h-20">
-                        <img src="/images/PortadaAlba.png" alt="portada revista" className="h-full"/>
+                    {LinkPreview({url: 'https://youtube.com/playlist?list=PLtDlpMIfDcOH4dt8BzlCKE-cFeC4WomAF'})}
+                    <h1 className="title uppercase group-hover:text-dorado">IL PODCAST DI DENISE</h1>
+                    <p>{getTextDate('2025-07-01')} </p>
+                    <h2 className="subtitle text-center italic group-hover:text-dorado">"Lo yoga come cammino di vita – La storia di Alba Muzzarelli"</h2>
+                    <a href="https://youtu.be/BpIY5-QJdk8?si=57mVAhZPL6aHHtqf" target="_blank" rel="noopener noreferrer" >
+                        <button className="btn-secondary">
+                            Ascolta tutto
+                        </button>
+                    </a>
+                </div>
+                <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
+                    {LinkPreview({url: 'https://www.glamour.mx/articulos/que-es-el-navakarana-yoga'})}
+                    <h1 className="title uppercase group-hover:text-dorado">GLAMOUR MEXICO</h1>
+                    <p>{getTextDate('2023-10-17')} </p>
+                    <h2 className="subtitle text-center italic group-hover:text-dorado">"Navakarana yoga, il metodo per fluire al ritmo della musica (e della luna)"</h2>
+                    <a href="https://www.glamour.mx/articulos/que-es-el-navakarana-yoga" target="_blank" rel="noopener noreferrer" >
+                        <button className="btn-secondary">
+                            Leggi tutto
+                        </button>
+                    </a>
+                </div>
+                <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
+                    {LinkPreview({url: 'https://vanityfair.it/article/navakarana-yoga-il-metodo-per-fluire-al-ritmo-di-musica-e-della-luna'})}
+                    <h1 className="title uppercase group-hover:text-dorado">VANITY FAIR</h1>
+                    <p>{getTextDate('2023-08-23')} </p>
+                    <a href="https://www.vanityfair.it/article/navakarana-yoga-il-metodo-per-fluire-al-ritmo-di-musica-e-della-luna" target="_blank" rel="noopener noreferrer" >
+                        <button className="btn-secondary">
+                            Leggi tutto
+                        </button>
+                    </a>
+                </div>
+                <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
+                    <div className="justify-items-center items-center mb-2">
+                        <img src={`https://t.prcdn.co/img?file=345m2022060700000000001001&page=1&width=240`} alt="Vivere lo yoga" className="w-28 h-28 rounded-3xl"/>
                     </div>
                     <h1 className="title uppercase group-hover:text-dorado">VIVERE LO YOGA</h1>
                     <p>{getTextDate('2022-06-07')} </p>
@@ -24,33 +75,11 @@ function Press() {
                     </a>
                 </div>
                 <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
-                    <img src="" alt="portada revista" />
+                    {LinkPreview({url: 'https://www.corrieredellospettacolo.net/2021/07/08/alba-muzzarelli-lo-yoga-come-stile-di-vita/'})}
                     <h1 className="title uppercase group-hover:text-dorado">CORRIERE DELLO SPETTACOLO</h1>
                     <p>{getTextDate('2021-07-21')} </p>
                     <h2 className="subtitle text-center italic group-hover:text-dorado">"Alba Muzzarelli, lo yoga come stile di vita"</h2>
                     <a href="https://www.corrieredellospettacolo.net/2021/07/08/alba-muzzarelli-lo-yoga-come-stile-di-vita/" target="_blank" rel="noopener noreferrer" >
-                        <button className="btn-secondary">
-                            Leggi tutto
-                        </button>
-                    </a>
-                </div>
-                <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
-                    <img src="" alt="portada revista" />
-                    <h1 className="title uppercase group-hover:text-dorado">MAT YOU CAN</h1>
-                    <p>{getTextDate('2022-09-29')} </p>
-                    <h2 className="subtitle text-center italic group-hover:text-dorado">"Alba Muzzarelli, insegnante di yoga online"</h2>
-                    <a href="https://youtu.be/IDRNmUcxX6c?si=k0Y1eJ16VK5CsVCB" target="_blank" rel="noopener noreferrer" >
-                        <button className="btn-secondary">
-                            Leggi tutto
-                        </button>
-                    </a>
-                </div>
-                <div className="flex-1 text-white group p-2 border-2 border-verdeOliva">
-                    <img src="" alt="portada revista" />
-                    <h1 className="title uppercase group-hover:text-dorado">Tu style</h1>
-                    <p>{getTextDate('2021-02-25')} </p>
-                    <h2 className="subtitle text-center italic group-hover:text-dorado">"E tu di che stile yoga sei?"</h2>
-                    <a href="https://www.tustyle.it/diary/yoga-maratona-online-stili-docenti-ceccarelli-lorenza-minola-loyoga-app-alba-muzzarelli/" target="_blank" rel="noopener noreferrer" >
                         <button className="btn-secondary">
                             Leggi tutto
                         </button>
