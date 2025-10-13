12c0e40b1377ef9b84159dad258b2f495f1c0a3c
ayelen <ayesorbellini@gmail.com>
2025-10-13 10:21:05 -0300

    Functionalize ContactForm

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,12 +2,12 @@ import ContactForm from "../components/ContactForm.jsx"
 
 function ContactUs() {
     return(
-        <div className="min-h-screen w-full">
+        <div className="min-h-dvh w-full">
             {/* Hero */}
-            <div className="relative min-h-screen w-full flex flex-col justify-center items-center text-oscuro bg-gradient-to-br from-dorado to-verdeBosque py-16" > 
-                <div className="z-20 px-12 py-8">
-                    <h1 className="textTitleSection tracking-wider uppercase py-4">Contatti </h1>
-                    <p className="textDetail max-w-3xl text-center">Hai delle domande? Vuoi maggiori informazioni sui nostri servizi? Compila il modulo di contatto qui sotto e ti risponderemo al più presto. Oppure qui sotto trovi i nostri contatti Instagram.</p>
+            <div className="relative w-full flex flex-col justify-center items-center text-white bg-gradient-to-br from-dorado to-verdeBosque py-16" > 
+                <div className="z-20 px-12 py-4 md:py-8">
+                    <h1 className="textTitleSection tracking-wider uppercase py-4 text-white ">Contatti </h1>
+                    <p className="textDetail max-w-3xl text-center text-white ">Hai delle domande? Vuoi maggiori informazioni sui nostri servizi? Compila il modulo di contatto qui sotto e ti risponderemo al più presto. Oppure qui sotto trovi i nostri contatti Instagram.</p>
                 </div>
                 <ContactForm />
             </div>
